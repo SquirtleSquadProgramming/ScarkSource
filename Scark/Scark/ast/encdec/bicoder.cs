87d5e1314a4f9ce4fcdd0d82c8771251ebf19988
Derek Chai <chderek9@gmail.com>
2019-05-07 17:58:15 +1200

    Moved ALL code from repl.it

---
+++ Scark/Scark/ast/encdec/bicoder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scark.ast.encdec
+{
+    public class BiCoder
+    {
+        public static void encStrToNum(string inputString)
+        {
+            byte[] input = Encoding.ASCII.GetBytes(inputString);
+            // List<int> output = new List<int>; 
+            foreach (int x in input)
+            {
+                Console.WriteLine(input[x].ToString());
+            }
+        }
+    }
+}
---
+++ Scark/Scark/ast/start/MenuSequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scark.ast.start
+{
+    public void menuSeq()
+    {
+        CC charcterCreation = new CC();
+        //Danny Doing Font Stuff and idk how it works plz halp
+        Console.WriteLine(""/*Ascii.Title*/);
+
+        Console.WriteLine("Press any key to begin...");
+        Console.ReadKey();
+        Console.WriteLine("1: New Game\n2: Load");
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "1":
+                //New Game Should be Called
+                //Call NewPlayer In CC
+                string[] tmp = charcterCreation.NewPlayer();
+                break;
+            case "2":
+                //Load Should be Called But It Might Not Idk... JK
+                //Danny Fanny Needs To Do Stuff Here
+                Console.WriteLine("u did smthng 2");
+                break;
+            default:
+                Console.WriteLine("Please input a valid number!");
+                //	menuSeq();
+                break;
+        }
+
+        // lord farquad is the good lorde
+        //Iron Man Dies Shreak 4
+        // ur mom is raped by ur pillowcase
+
+    }
+}
