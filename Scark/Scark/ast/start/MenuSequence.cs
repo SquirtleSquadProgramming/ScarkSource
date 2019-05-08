c27c7bd548122a817522b235496958ad8d4e5caa
BeefStew <34359784+BeefStew34@users.noreply.github.com>
2019-05-08 18:54:08 +1200

    Update MenuSequence.cs

--- Scark/Scark/ast/start/MenuSequence.cs
+++ Scark/Scark/ast/start/MenuSequence.cs
@@ -17,6 +17,7 @@ namespace Scark.ast.start
             Console.WriteLine("\nPress any key to begin...");
             Console.ReadKey();
             Console.Clear();
+            Console.WriteLine("      #######                                         #    \n    #       ###                                     ##  \n   #         ##                                     ##\n   ##        #                                      ## \n    ###                                ###  ####    ##\n   ## ###           ####       ####     #### #### # ##  ###  \n    ### ###        # ###  #   # ###  #   ##   ####  ## # ###\n      ### ###     #   ####   #   ####    ##         ###   # \n        ### ###  ##         ##    ##     ##         ##   # \n          ## ### ##         ##    ##     ##         ##  #\n           ## ## ##         ##    ##     ##         ## ## \n            # #  ##         ##    ##     ##         ###### \n  ###        #   ###     #  ##    ##     ###        ##  ### \n #  #########     #######    ##### ##     ###       ##   ### #\n#     #####        #####      ###   ##               ##   ###\n#      \n ## ");
             Console.WriteLine("\n[1] New Game\n[2] Load");
             string input = Console.ReadLine();
 
