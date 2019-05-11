d334785fc4ccdc2324fff863f9cbba80b6995b0e
kylerosser <kyle221rosser@xtra.co.nz>
2019-05-11 12:47:14 +1200

    Commited Nathans disableQuickEdit
    Implemented it without merging a new branch because Nathans branch had problems with the .csproject file

--- Scark/Scark/Program.cs
+++ Scark/Scark/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Scark.ast;
+using System.Runtime.InteropServices;
 
 namespace Scark
 {
@@ -12,8 +13,50 @@ namespace Scark
     {
         public readonly static string gameVersion = "v0.1.5-alpha"; // game version variable
 
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+
+        // STD_INPUT_HANDLE (DWORD): -10 is the standard input device.
+        const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        internal static bool Go()
+        {
+
+            IntPtr consoleHandle = GetStdHandle(STD_INPUT_HANDLE);
+
+            // get current console mode
+            uint consoleMode;
+            if (!GetConsoleMode(consoleHandle, out consoleMode))
+            {
+                // ERROR: Unable to get console mode.
+                return false;
+            }
+
+            // Clear the quick edit bit in the mode flags
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+
+            // set the new mode
+            if (!SetConsoleMode(consoleHandle, consoleMode))
+            {
+                // ERROR: Unable to set console mode
+                return false;
+            }
+
+            return true;
+        }
+
         public static void Main(string[] args)
         {
+            Go();
+
             //Used To Remove Warnings 
             Console.Clear();
             // converting the start main function to a non - static
