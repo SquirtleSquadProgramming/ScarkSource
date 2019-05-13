82f1a865faffd698fea6f6af8cbaaa095179c332
MinnowTheFish <danieleded2@gmail.com>
2019-05-13 19:29:24 +1200

    Possibly Complexity Fix

--- Scark/Scark/Character.cs
+++ Scark/Scark/Character.cs
@@ -268,26 +268,6 @@ namespace Scark
 
         #region Ability Scores
 
-        private static string abbreviationToName(string input)
-        {
-            switch (input)
-            {
-                case "CON":
-                    return "constitution"; // Setting it to* constitution
-                case "CHA":
-                    return "charisma"; // * charisma
-                case "INT":
-                    return "intelligence"; // * intelligence
-                case "PER":
-                    return "perception"; // * perception
-                case "STR":
-                    return "strength"; // * strength
-                case "STE":
-                    return "stealth"; // * stealth
-            }
-            throw new NotImplementedException();
-        }
-
         // Void for the choose ability score points menu
         public static void chooseAbilityScorePoints()
         {
@@ -295,130 +275,32 @@ namespace Scark
 
             bool optionPicked = false; // setting optionPicked to false for later use in a while loop
 
-            //Clearing the Console and printing the options
-            Console.Clear();
-            Console.Write("                             {0} Ability Points Remaining\n═══════════════════════════════════════════════════════════════════════════════════\n                                Options:\n[STE] Stealth      : Likelihood of surprising people, avoiding danger, hiding, etc.\n[CON] Constitution : Determines HP, likelyhood of surviving sickness, etc.\n[INT] Intelligence : Ability to know about things, traps, enemies, etc.\n[CHA] Charisma     : Likelihood of persuading or other speech actions.\n[PER] Perception   : Ability to sense traps, find clues, etc.\n[STR] Strength     : Ability to use strength\n[ X ]   Exit         : Exit this menu\nPlease select an ability to add points to (Max 25 points to each ability):\n> ", abilityPoints);
+            string addTo = Scark.ast.Other.CASP.Category();
+            if (addTo == "exit") return;
 
-            // string addTo processed to give a maximum of 3 characters
-            string addTo = (Console.ReadLine().Replace("[", "").Replace("]", "").ToUpper() + "   ").Substring(0, 3).Replace(" ", "");
+            
+            Console.Clear(); //Clearing the Console and printing the options
 
-            // Processing the (processed) input to the actual name of an ability for later use
-            if (addTo == "X") return;
-            else
-            {
-                while (!optionPicked)
-                {
-                    optionPicked = true; // Exiting the while loop
-                    try
-                    {
-                        // Changing the abbreviation to the proper name
-                        addTo = abbreviationToName(addTo);
-                    }
-                    catch (NotImplementedException e)
-                    {
-                        optionPicked = false; // Making the while loop, loop again
-                        // Re asking
-                        Console.Write("                              That is not an option\n                             {0} Ability Points Remaining\n═══════════════════════════════════════════════════════════════════════════════════\n                                Options:\n[STE] Stealth      : Likelihood of surprising people, avoiding danger, hiding, etc.\n[CON] Constitution : Determines HP, likelyhood of surviving sickness, etc.\n[INT] Intelligence : Ability to know about things, traps, enemies, etc.\n[CHA] Charisma     : Likelihood of persuading or other speech actions.\n[PER] Perception   : Ability to sense traps, find clues, etc.\n[STR] Strength     : Ability to use strength\n[ X ]   Exit         : Exit this menu\nPlease select an ability to add points to (Max 25 points to each ability):\n> ", abilityPoints);
-                        addTo = (Console.ReadLine().Replace("[", "").Replace("]", "").ToUpper() + "   ").Substring(0, 3).Replace(" ", "");
-                    }
-                }
-            }
-
-            // Asking the amound of points wants to add to the specified ability
+            // Asking the amount of points wants to add to the specified ability
             Console.Write("═══════════════════════════════════════════════════════════════════════════════════\nPlease enter the amount of points that want to add to the {0} abilty:\n> ", addTo);
 
-            // parsing the input
-            optionPicked = false;
-            int amount = 0;
-            while(!optionPicked)
-            {
-                optionPicked = true;
-                try
-                {
-                    amount = Math.Abs(Int32.Parse(Console.ReadLine()));
-                }
-                catch (Exception e)
-                {
-                    optionPicked = false;
-                    Console.Write("                          Please enter a number!\n═══════════════════════════════════════════════════════════════════════════════════\nPlease enter the amount of points that want to add to the {0} abilty:\n> ", addTo);
-                }
-            }
-                
-            // loop while the user has exceeded: the amount of abilityPoints they have
-            //                                   the max amount of points an ability can have
-            while (!(amount <= abilityPoints && amount + AbilityScores[addTo] <= 25))
-            {
-                // Writing that they have exceeded an amount
-                Console.Write("═══════════════════════════════════════════════════════════════════════════════════\n                        {1} exceeds either: {2} or 25!\nPlease enter the amount of points that want to add to the {0} abilty:\n> ", addTo, amount, abilityPoints);
-
-                // Re-getting their input
-                try
-                {
-                    amount = Math.Abs(Int32.Parse(Console.ReadLine()));
-                }
-                catch (Exception e)
-                {
-                    Console.Write("                             That is not a number!\n");
-                }
-            }
+            int amount = Scark.ast.Other.CASP.Amount();
 
             // Adding the amount to the selected ability
             AbilityScores[addTo] += amount;
-
             // Subtracting amount from the abilityPoints
             abilityPoints -= amount;
 
-            // setting optionPicked to false as to reuse it
-            optionPicked = false;
-            dynamic apply = false; // dynamic variable apply to false
+            bool apply = Scark.ast.Other.CASP.ApplyChanges();
 
-            // Looping until the user picks Y/N
-            while (optionPicked == false)
+            if (!apply)
             {
-                // Asking if they wish to apply the changes
-                Console.Write("═══════════════════════════════════════════════════════════════════════════════════\nDo you wish to apply these changes: Add {0} to {1} leaving you with {2}\n[Y] Apply\n[N] Revert changes\n> ", amount, addTo, abilityPoints);
+                abilityPoints += amount; // Reversing changes
+                AbilityScores[addTo] -= amount; // Reversing changes
+            }
 
-                //Getting and processing their input to only 1 character
-                apply = Console.ReadLine().Replace("[", "").Replace("]", "").Replace(" ", "").Substring(0, 1).ToUpper();
-
-                switch (apply)
-                {
-                    case "Y": // If they picked yes
-                        optionPicked = true; // Exiting while loop
-                        apply = true; // Setting apply(dynamic) to true
-                        break;
-                    case "N": // If they picked no
-                        optionPicked = true; // Exiting while loop
-                        abilityPoints += amount; // Reversing changes
-                        AbilityScores[addTo] -= amount; // Reversing changes
-                        apply = false; // Setting apply(dynamic) to false
-                        break;
-                }
-            }
-              
-            // Asking if they wish to use more ability points
-            Console.Write("═══════════════════════════════════════════════════════════════════════════════════\n                Do you wish to use more ability points?\n                [Y] Yes I do!            No Thanks! [N]\n> ");
-                
-            // Getting their input and processing it to 1 character
-
-            string inp = Console.ReadLine().Replace("[", "").Replace("]", "").Replace(" ", "").ToUpper();
-            try
-            {
-                inp = inp.Substring(0, 1);
-            }
-            catch
-            {
-                inp = "N";
-            }
-            switch (inp)
-            {
-                case "Y": // if they did wish to use more
-                    break;
-                case "N": // if they didn't wish to use more
-                    return;
-                default:
-                    return;
-            }
+            if (!Scark.ast.Other.CASP.UseMorePoints())
+                return;
 
             chooseAbilityScorePoints();
         }
