ad453a7bdff4f8f7fe92fc1213383b568321ddef
Derek Chai <chderek9@gmail.com>
2019-05-14 13:04:33 +1200

    Update BoardShip.cs

--- Scark/Scark/ast/start/BoardShip.cs
+++ Scark/Scark/ast/start/BoardShip.cs
@@ -42,7 +42,7 @@ namespace Scark.ast.start
             Character.wd("[LORD WAKEHART] Oh my, could it be!");
             Character.wd("Wakehart reaches into the rack, and takes out a large bottle.");
             Character.wd("[LORD WAKEHART] 43%, aged scotch!");
-            Character.wd("[LORD WAKEHART] For what do ye wish to trade with?");
+            Character.wd("[LORD WAKEHART] For what do you wish to trade with?");
             Character.wd("[THORUL] 25 Ethryl, for this was quite the doozy to get ahold of.");
             Character.wd("Wakehart hands a bag over to Thorul, and leaves the deck grinning.");
             Character.wd("Other crew members make their trades, and exit the deck.");
@@ -51,11 +51,11 @@ namespace Scark.ast.start
             Character.wd("[THORUL] How about this scroll of wisdom.");
             Character.wd("[THORUL] It was gifted to me by a man over at Narsk.");
             Character.wd("[THORUL] When chanted, the spell enchants you with wisdom and intelligence.");
-            Character.wd("[THORUL] However, it only works on humankind. Which is a shame for a dwarf like myself.");
+            Character.wd("[THORUL] However, it only works on humankind, which is quite a shame for a dwarf like myself.");
             Character.wd("[THORUL] I will give it to you if you polish my balmorals.");
 
             Character.wd($"[1] I accept your trade.\n[2] I do not accept your trade.");
-            Console.Write(">");
+            Console.Write("> ");
             string response = Console.ReadLine();
 
             switch (response)
@@ -67,6 +67,7 @@ namespace Scark.ast.start
                     Character.wd("Your intelligence ability increases by one.");
                     Character.AbilityScores["intelligence"]++;
                     Character.wd("You spend the rest of the day polishing Thorul's shoes before heading back inside to sleep.");
+                    // wow those must be some massive shoes
                     Character.wd("Thorul departed just after nightfall.");
                     break;
                 case "2":
@@ -74,18 +75,18 @@ namespace Scark.ast.start
                     Character.wd("[THORUL] That is alright.");
                     Character.wd("[THORUL] Stay safe, friend.");
                     Character.wd("You go back inside to have an early nap.");
-                    Character.wd("Thorul departs.");
+                    Character.wd("Thorul and his crew departs the berth.");
                     break;
             }
             Character.pressAnyKeyToContinue();
 
-            Character.wd("\nA week passes onboard the Farquaad.");
+            Character.wd("\nAnother week passes.");
             Character.wd("[LORD WAKEHART] Hey kid.");
             Character.wd("[LORD WAKEHART] Not long 'til the Farquaad arrives at Scark.");
             Character.wd("[LORD WAKEHART] She's gonna stay in the port for a lil' while as we restock.");
             Character.wd("[LORD WAKEHART] Y'know what, go to the town called Narsk not far from here, and go to the tarven called \"The Medallion\".");
             Character.wd("[LORD WAKEHART] Talk to my friend Orpheus, he's the barman. He'll tell you some stuff you ought to know.");
-            Character.wd("[LORD WAKEHART] Also, ethryl is the currency of Scark.");
+            Character.wd("[LORD WAKEHART] Also, if you didn't know already, ethryl is the currency of Scark.");
             Character.wd("[LORD WAKEHART] Here's some of it to start you off.");
             Character.wd("Wakehart hands over a few coins radiating with a strange light.");
 
