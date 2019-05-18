e34bda6b924682157e8107f10c98f0f889fb60dc
MinnowTheFish <danieleded2@gmail.com>
2019-05-18 23:50:58 +1200

    Fixed #31
    Changed MenuSequence.cs:
     - internal class menuSequenceAntiComplex
     - Broke up old code into small (less cyclomatically complex) pieces

--- Scark/Scark/ast/start/MenuSequence.cs
+++ Scark/Scark/ast/start/MenuSequence.cs
@@ -47,58 +47,66 @@ namespace Scark.ast.start
             Console.ReadKey();
 
             //Instantiate classes for later
-            story Story = new story();
-            SettingsMenu settingsMenu = new SettingsMenu();
+            menuSequenceAntiComplex.main(colours, chosenIndex);
+        }
 
-            //Option selection
-            bool optionSelected = false;
+        internal static class menuSequenceAntiComplex
+        {
+            internal static void main(ConsoleColor[] colours, int chosenIndex)
+            {
+                //Option selection
+                bool optionSelected = false;
 
-            while (optionSelected == false)
+                while (optionSelected == false)
+                {
+                    menuSequenceAntiComplex.prints(colours, chosenIndex);
+                    optionSelected = run((Console.ReadLine().ToLower().Replace(" ", "") + "   ").Substring(0, 3).Replace(" ", ""));
+                }
+            }
+
+            internal static void prints(ConsoleColor[] colours, int chosenIndex)
             {
                 Console.Clear();
 
                 //Print the title with the random colour chosen (if special effects is enabled)
-                if (Character.Settings["SpecialEffects"])
-                {
-                    Console.ForegroundColor = colours[chosenIndex];
-                }
-                Console.WriteLine("      ███████                                         █    \n    █       ███                                     ██  \n   █         ██                                     ██\n   ██        █                                      ██ \n    ███                                ███  ████    ██\n   ██ ███           ████       ████     ████ ████ █ ██  ███  \n    ███ ███        █ ███  █   █ ███  █   ██   ████  ██ █ ███\n      ███ ███     █   ████   █   ████    ██         ███   █ \n        ███ ███  ██         ██    ██     ██         ██   █ \n          ██ ███ ██         ██    ██     ██         ██  █\n           ██ ██ ██         ██    ██     ██         ██ ██ \n            █ █  ██         ██    ██     ██         ██████ \n  ███        █   ███     █  ██    ██     ███        ██  ███ \n █  █████████     ███████    █████ ██     ███       ██   ███ █\n█     █████        █████      ███   ██               ██   ███\n█      \n ██ ");
+                if (Character.Settings["SpecialEffects"])Console.ForegroundColor = colours[chosenIndex];
 
+                Console.WriteLine("      ███████                                         █    \n    █       ███                                     ██  \n   █         ██                                     ██\n   ██        █                                      ██ \n    ███                                ███  ████    ██\n   ██ ███           ████       ████     ████ ████ █ ██  ███  \n    ███ ███        █ ███  █   █ ███  █   ██   ████  ██ █ ███\n      ███ ███     █   ████   █   ████    ██         ███   █ \n        ███ ███  ██         ██    ██     ██         ██   █ \n          ██ ███ ██         ██    ██     ██         ██  █\n           ██ ██ ██         ██    ██     ██         ██ ██ \n            █ █  ██         ██    ██     ██         ██████ \n  ███        █   ███     █  ██    ██     ███        ██  ███ \n █  █████████     ███████    █████ ██     ███       ██   ███ █\n█     █████        █████      ███   ██               ██   ███\n█      \n ██ ");
                 Character.revertColourScheme();
 
                 if (Character.Loaded) Console.Write("\n                 Start      Settings    Exit\n           =[ 1 ]=    =[ 2 ]=    =[ 3 ]=   =[ x ]=\n> "); // Options
                 else Console.Write("\n             New       Load      Settings    Exit\n           =[ 1 ]=    =[ 2 ]=    =[ 3 ]=   =[ x ]=\n> "); // Options
 
                 if (Character.dev == true) Console.Write("Dev mode enabled\n> "); // Saying if dev mode is enabled
+            }
 
-                switch (Console.ReadLine())
+            internal static bool run(string option)
+            {
+                switch (option)
                 {
                     case "1": //New Game
-                        optionSelected = true;
-                        Story.Run();
-                        break;
+                        new ast.story().Run();
+                        return true;
                     case "dev": //Dev mode (hidden)
                         if (Character.dev == false) Character.dev = true;
                         else Character.dev = false;
-                        break;
+                        return false;
                     case "2": //Load character (doesn't work yet)
-                        optionSelected = true;
                         Console.WriteLine("\nThis feature is currently WIP");
-                        Thread.Sleep(Character.Settings["SpeechSpeed"]);
+                        Character.pressAnyKeyToContinue();
                         Console.Clear();
-                        menuSeq();
-                        break;
+                        return false;
                     case "3": //Settings
-                        settingsMenu.run();
-                        break;
+                        new SettingsMenu().run();
+                        return false;
                     case "x": //Exit
-                        optionSelected = true;
                         Character.stage = -1;
-                        return;
+                        return true;
                     default: //for when they try to be smartasses
                         Console.WriteLine("Please input a valid number!");
-                        break;
+                        return false;
                 }
+                throw new NotImplementedException();
             }
         }
     }
