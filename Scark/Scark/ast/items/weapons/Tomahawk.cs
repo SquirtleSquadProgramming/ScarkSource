26e0276fa8a81f0255acaff093ae24a5116c4400
kylerosser <kyle221rosser@xtra.co.nz>
2019-08-03 21:55:24 +1200

    Added class
    Added Tomohawk.cs

---
+++ Scark/Scark/ast/items/weapons/Tomahawk.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scark.ast.items;
+
+namespace Scark.ast.items.weapons
+{
+    static class Tomahawk
+    {
+        public static Dictionary<string, dynamic> Attributes = new Dictionary<string, dynamic>()
+        {
+            { "Damage", 6 }, // WIP
+            { "Class", "Melee" } // WIP
+        };
+        public static int ID = 0;
+        public static int Price = 100; // Need to Change!
+        public static string Name = "Tomahawk";
+        public static string Description = "";
+        public static string Image = @"                '
+               / \
+              /   \
+             /  `  \
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  |  |
+             |  `  |
+ \           |     |           /
+  '-.._____..`.....`.._____..-'
+  \  ---__     [0]     __---  /
+   '.________..---..________.'
+              |___|
+              |___|
+              |___|
+              |___|
+              |___|";
+
+        public static Item ToItem() => new Item(ID, Price, Name, Description, Image, Attributes);
+    }
+}
